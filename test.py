44a3242306b409b2a1ebb2ce75098d1620d74b0f
Molcan <uig76644@contiwan.com>
2024-09-16 07:53:22 +0200

    Secret fixed

--- test.py
+++ test.py
@@ -1,15 +1,22 @@
-import torch
-
 # Use a pipeline as a high-level helper
 from transformers import pipeline
-# import huggingface_hub
+from dotenv import load_dotenv
+import torch
 import os
 
-# print(os.getenv("HUGGING_FACE_API_KEY"))
-# exit()
+load_dotenv()  # This will load the environment variables from the .env file
 
-# huggingface_hub.set_auth_token(os.getenv("HUGGING_FACE_API_KEY"))
-pipe = pipeline("text-generation", model="meta-llama/Meta-Llama-3-8B", token="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")
+pipe = pipeline(
+    "text-generation",
+    model_kwargs={"torch_dtype": torch.bfloat16},
+    model="meta-llama/Meta-Llama-3-8B",
+    token=os.getenv("HUGGING_FACE_API_KEY")
+)
+
+chat_template = """
+[USER_ROLE] "{USER_CONTENT}"
+[ASSISTANT_ROLE] "{ASSISTANT_CONTENT}"
+"""
 
 messages = [
     {"role": "system", "content": "You are a pirate chatbot who always responds in pirate speak!"},
@@ -22,12 +29,9 @@ terminators = [
 ]
 
 outputs = pipe(
-    messages,
-    max_new_tokens=256,
-    eos_token_id=terminators,
-    do_sample=True,
-    temperature=0.6,
-    top_p=0.9,
+    messages
 )
+
 assistant_response = outputs[0]["generated_text"][-1]["content"]
+
 print(assistant_response)
